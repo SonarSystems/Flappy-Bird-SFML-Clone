e4bd0d836c052d86807e016ab626eeac7581e2e8
Sonar Systems <admin@sonarsystems.co.uk>
2017-02-01 23:18:57 +0000

    Mo Code

--- Code/STATE CREATION/8 - Main Menu State/DEFINITIONS.hpp
+++ Code/STATE CREATION/8 - Main Menu State/DEFINITIONS.hpp
@@ -6,3 +6,7 @@
 #define SPLASH_STATE_SHOW_TIME 3.0
 
 #define SPLASH_SCENE_BACKGROUND_FILEPATH "Resources/res/Splash Background.png"
+#define MAIN_MENU_BACKGROUND_FILEPATH "Resources/res/sky.png"
+
+#define GAME_TITLE_FILEPATH "Resources/res/title.png"
+#define PLAY_BUTTON_FILEPATH "Resources/res/PlayButton.png"
